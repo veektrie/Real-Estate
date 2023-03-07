e03d69c4c207430518abf1d64bdabc23891ebbac
Adugbo Victory <adugbovictory@gmail.com>
2023-03-07 17:35:10 +0100

    pie chart

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useList } from '@pankod/refine-core';
+
+import {
+  Typography,Box,Stack
+} from '@pankod/refine-mui';
+
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from 'components';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color='#11142d' >
+        Dashboard
+      </Typography>
+      <Box mt='20px' display='flex' flexWrap='wrap' gap={4}>
+        <PieChart
+          title="Properties for Sale"
+          value={684}
+          series={[75, 25]}
+          colors={['#475be8','#e4e8ef']}
+        />
+        <PieChart
+          title="Properties for Rent"
+          value={550}
+          series={[60, 40]}
+          colors={['#472de8','#e4f9ef']}
+        />
+        <PieChart
+          title="Total customers"
+          value={15000}
+          series={[70, 30]}
+          colors={['#476ce8','#e4d4ef']}
+        />
+        <PieChart
+          title="Total Cities"
+          value={350}
+          series={[75, 25]}
+          colors={['#475be8','#e4e8ef']}
+        />
+      </Box>
+    </Box>
   )
 }
 
